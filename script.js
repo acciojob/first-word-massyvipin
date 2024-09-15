066c6f43ed92536f99356398bbe32a11a74ed3ba
massyvipin <166917148+massyvipin@users.noreply.github.com>
2024-09-15 21:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+ s = s.trim();
+	const spaceIndex = s.indexOf(' ');
+	if(spaceIndex=== -1){
+		return s;	
+	}
+	return s.subsrting(0,spaceIndex);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+/alert(firstWord(s));
